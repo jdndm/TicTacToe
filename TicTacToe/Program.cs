b1d02836a7aeb9714d4c173beb2f2e240fb521ea
Jack McQuinn <Jack.McQuinn@unisuper.com.au>
2019-01-23 15:42:09 +1100

    Changed appearance of the gameboard.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -20,15 +20,16 @@ namespace TicTacToe
         public static void Print(string[] board) 
         {
             Console.Clear();
+            Console.WriteLine("PLAYER1: X PLAYER2: O");
             Console.WriteLine();
-            Console.WriteLine("    |    |    ");
-            Console.WriteLine("  {0} |  {1} |  {2}  ", board[0], board[1], board[2]);
-            Console.WriteLine("____|____|____");
-            Console.WriteLine("    |    |    ");
-            Console.WriteLine("  {0} |  {1} |  {2}  ", board[3], board[4], board[5]);
-            Console.WriteLine("____|____|____");
-            Console.WriteLine("    |    |    ");
-            Console.WriteLine("  {0} |  {1} |  {2}  ", board[6], board[7], board[8]);
+            Console.WriteLine("       |    |    ");
+            Console.WriteLine("     {0} |  {1} |  {2}  ", board[0], board[1], board[2]);
+            Console.WriteLine("   ____|____|____");
+            Console.WriteLine("       |    |    ");
+            Console.WriteLine("     {0} |  {1} |  {2}  ", board[3], board[4], board[5]);
+            Console.WriteLine("   ____|____|____");
+            Console.WriteLine("       |    |    ");
+            Console.WriteLine("     {0} |  {1} |  {2}  ", board[6], board[7], board[8]);
             Console.WriteLine();
         }
 
