0649a47ed407fa82100ca1df6a58e533adbd0e2e
Jack McQuinn <Jack.McQuinn@unisuper.com.au>
2019-01-24 13:19:54 +1100

    Added WiningCondition() method, and implemented logic into Main()

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -16,7 +16,6 @@ namespace TicTacToe
             }
             return board;
         }
-
         public static void Print(string[] board) 
         {
             Console.Clear();
@@ -32,7 +31,6 @@ namespace TicTacToe
             Console.WriteLine("     {0} |  {1} |  {2}  ", board[6], board[7], board[8]);
             Console.WriteLine();
         }
-
         public static string [] Player(bool player1)
         {
             string[] player = new string [2];
@@ -48,15 +46,12 @@ namespace TicTacToe
             }
             return player;
         }
-
         public static string [] Move(string[] board, bool player1)
         {
             int validMove = 0;
+            string[] player = Player(player1);
             while (validMove != 1)
             {
-
-                string [] player = Player(player1);
-                
 
                 Console.WriteLine("{0}, choose where you want to place a move..", player[0]);
                 int move = (int.Parse(Console.ReadLine())) - 1;
@@ -64,8 +59,6 @@ namespace TicTacToe
                 {
                     board[move] = player[1];
                     validMove = 1;
-
-
                 }
                 else
                 {
@@ -76,24 +69,67 @@ namespace TicTacToe
                 
             }
             return board;
-        }         
+        } 
+        public static bool WinningCondition(string [] board)
+        {
+            bool win;
+            if (board[0] == board[1] && board[1] == board[2])
+            {
+                win = true;
+            }
+            else if (board[3] == board[4] && board[4] == board[5])
+            {
+                win = true;
+            }
+            else if (board[6] == board[7] && board[7] == board[8])
+            {
+                win = true;
+            }
+            else if (board[0] == board[3] && board[3] == board[6])
+            {
+                win = true;
+            }
+            else if (board[1] == board[4] && board[4] == board[7])
+            {
+                win = true;
+            }
+            else if (board[2] == board[5] && board[5] == board[8])
+            {
+                win = true;
+            }
+            else if (board[0] == board[4] && board[4] == board[8])
+            {
+                win = true;
+            }
+            else if (board[6] == board[4] && board[4] == board[2])
+            {
+                win = true;
+            }
+            else
+            {
+                win = false;
+            }
+            return win;
+        }
     }
     class Program
-    {
-  
+    { 
         static void Main(string[] args)
         {
             bool player1 = true;
+            bool win = false;
             string [] board = gameBoard.Create();
-
             gameBoard.Print(board);
-            for (int i = 0; i < 3; i++)
+            do
             {
                 gameBoard.Move(board, player1);
                 gameBoard.Print(board);
-                player1 = !player1;
-            }
-            
+                win = gameBoard.WinningCondition(board);
+                if (win == false)
+                {
+                    player1 = !player1;
+                }
+            } while (win == false);
 
             Console.ReadKey();
         }
